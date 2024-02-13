58a546bed9e5f7a402958bcb830cd504e2c3516d
wn1331 <wn1331@gmail.com>
2024-02-13 10:10:07 +0900

    feat : 나머지 주석

--- monologicServer/src/main/java/msa/project/monologicserver/domain/report/ReportRepository.java
+++ monologicServer/src/main/java/msa/project/monologicserver/domain/report/ReportRepository.java
@@ -1,9 +1,9 @@
-package msa.project.monologicserver.domain.report;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReportRepository extends JpaRepository<Report,Long> {
-
-}
+//package msa.project.monologicserver.domain.report;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface ReportRepository extends JpaRepository<Report,Long> {
+//
+//}
--- monologicServer/src/main/java/msa/project/monologicserver/domain/review/ReviewRepository.java
+++ monologicServer/src/main/java/msa/project/monologicserver/domain/review/ReviewRepository.java
@@ -1,9 +1,9 @@
-package msa.project.monologicserver.domain.review;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReviewRepository extends JpaRepository<Review,Long> {
-
-}
+//package msa.project.monologicserver.domain.review;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface ReviewRepository extends JpaRepository<Review,Long> {
+//
+//}
