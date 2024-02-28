3464d2fce43b80b0b91499398530a3541e4a78d3
wn1331 <wn1331@gmail.com>
2024-02-28 15:55:13 +0900

    feat : D

--- monologicServer/src/main/java/msa/project/monologicserver/api/controller/ProductController.java
+++ monologicServer/src/main/java/msa/project/monologicserver/api/controller/ProductController.java
@@ -1,11 +1,14 @@
 package msa.project.monologicserver.api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import msa.project.monologicserver.api.dto.req.product.ProductInsertRequestDTO;
 import msa.project.monologicserver.application.ProductService;
 import msa.project.monologicserver.global.ApiResponse;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,10 +22,19 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping("/")
+    @Operation(summary = "C", description = "상품 등록")
     public ApiResponse<String> addProduct(
         @Valid @ModelAttribute ProductInsertRequestDTO requestDTO
     ) {
-
         return ApiResponse.success(productService.addProduct(requestDTO));
     }
+
+    @DeleteMapping("/{productId}")
+    @Operation(summary = "D", description = "상품 제거")
+    public ApiResponse<String> deleteProduct(
+        @Valid @PathVariable String productId
+    ) {
+        return ApiResponse.success(productService.deleteProduct(requestDTO));
+    }
+
 }
--- monologicServer/src/main/java/msa/project/monologicserver/global/validation/ListValidation.java
+++ monologicServer/src/main/java/msa/project/monologicserver/global/validation/ListValidation.java
@@ -1,14 +1,16 @@
 package msa.project.monologicserver.global.validation;
 
 import java.util.List;
+import msa.project.monologicserver.domain.member.Member;
 import msa.project.monologicserver.global.error.code.CommonErrorCode;
 import msa.project.monologicserver.global.error.exception.BusinessException;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public class ListValidation {
 
-    public static void listValidation(List<?> lists){
-        if(lists==null||lists.isEmpty()){
+    public static void listValidation(List<?> lists) {
+
+        if (lists == null || lists.isEmpty()) {
             throw new BusinessException(CommonErrorCode.LIST_CANNOT_EMPTY);
         }
     }
