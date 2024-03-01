860f4d65f05d48b73b31c4a7f08edda6335199be
DESKTOP-MJ5PTK3\SoraCyan <yskh95s@naver.com>
2024-03-02 06:50:46 +0900

    목록조회 되는것 확인

--- monologicServer/src/main/java/msa/project/monologicserver/api/controller/ProductContoller.java
+++ monologicServer/src/main/java/msa/project/monologicserver/api/controller/ProductContoller.java
@@ -3,10 +3,11 @@ package msa.project.monologicserver.api.controller;
 import lombok.RequiredArgsConstructor;
 import msa.project.monologicserver.api.dto.req.product.ProductDTO;
 import msa.project.monologicserver.application.ProductService;
+import msa.project.monologicserver.domain.product.entity.Product;
 import msa.project.monologicserver.global.ApiResponse;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
@@ -15,8 +16,13 @@ public class ProductContoller {
 
     private final ProductService productService;
     @PostMapping("/")
-    public ApiResponse<String> resist(ProductDTO pdto) {
+    public ApiResponse<String> resist(@RequestBody ProductDTO pdto) {
         return ApiResponse.success(productService.resist(pdto));
+    }
+
+    @GetMapping("/")
+    public List<Product> productList() {
+        return productService.getList();
     }
 
 
--- monologicServer/src/main/java/msa/project/monologicserver/application/ProductService.java
+++ monologicServer/src/main/java/msa/project/monologicserver/application/ProductService.java
@@ -6,6 +6,8 @@ import msa.project.monologicserver.domain.product.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 import msa.project.monologicserver.domain.product.entity.Product;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ProductService {
@@ -15,4 +17,11 @@ public class ProductService {
         Product product = productRepository.save(pdto.toProduct());
         return product.getTitle();
     }
+
+    public List<Product> getList() {
+        List<Product> allprocuctList = productRepository.findAll();
+
+        return allprocuctList;
+    }
+
 }
